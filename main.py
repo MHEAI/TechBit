6619140233576b7a9fd5f84cdd4f7f3e58c7082f
= <m.haadi.ehsan@gmail.com>
2025-07-28 21:48:56 +1000

    Added Voice Over Functionality to Main

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from Classes.uploader import Uploader
 from Classes.photo_finder import PhotoFinder
 from Classes.video_editor import VideoEditor
 from Classes.voice_over import VoiceOver
-
+from Classes.utils import Utilities
 
 def main():
     
+    Utilizer = Utilities()
     scriptor = Scriptor()
     code_generator = CodeGenerator()
     uploader = Uploader()
@@ -15,12 +16,22 @@ def main():
     video_editor = VideoEditor()
     voice_over = VoiceOver()
     
-    topic = "a simple random generator game"
+    topic = "any cool python fact"
     
+    Utilizer.ensure_ollama_running()
     scriptor.generate_script(topic)
     
-    topic, hook,intro, text,visual, code, emphasize_words,outro = scriptor.seperate_characters()
+    topic, hook,intro, problem_text,text,visual, code, emphasize_words,outro = scriptor.seperate_items("script.json")
     
+    
+    topic_audio = voice_over.create_voiceover(topic,"topic")
+    hook_audio = voice_over.create_voiceover(hook,"hook")
+    intro_audio = voice_over.create_voiceover(intro,"intro")
+    problem_text_audio = voice_over.create_voiceover(problem_text,"problem_text")
+    text_audio = voice_over.create_voiceover(text,"text")
+    outro_audio = voice_over.create_voiceover(outro,"outro")
+    
+
     # voice_over_path = voice_over.generate_voiceover(topic,hook,intro,text,emphasize_words,outro)
     
     # code_snippet = code_generator.generate_code_snippet(code)
